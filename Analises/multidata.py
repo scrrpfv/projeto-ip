8d26c1e4c06bafff11d182f863fb0a56e788738f
tome <tcl@cin.ufpe.br>
2024-03-18 13:46:16 -0300

    Adicionada Funcionalidade de projeção ao multidata

--- Analises/multidata.py
+++ Analises/multidata.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import nbformat
 import copy
+import statsmodels.api as sm
 
 
 class MultiData():
@@ -25,7 +26,7 @@ class MultiData():
     # Formatação para printar os nomes dos dataframes
     def __str__(self):
         return f'{" | ".join(self.names)}'
-            
+    
 
     def autoplot(self, index, tamanho=(8,8), n=0):
         df = self[index]
@@ -49,9 +50,14 @@ class MultiData():
         self.plot_selection(df, pick, decorado)
 
 
-    def plot_selection(self, df, pick, decorado=False):
+    def plot_selection(self, df, pick=[], decorado=False):
+        if len(pick) == 0:
+            pick = df.columns.values
+        
         if not decorado:
             self.decorate(n=len(pick))
+        
+        
         for i in df.columns:
             if i in pick:
                 plt.plot(df.index.values, df[i], label=i)
@@ -69,29 +75,53 @@ class MultiData():
         self.ax.set_prop_cycle('color', plt.cm.nipy_spectral(np.linspace(0, 1, n)))
         
         
-    def change_to_DataTable(self, df, name):
-        df_copy = DataTable(columns = df.columns, data = copy.deepcopy(df.values), name=name)
-        return df_copy
+    def projection_var(self, training_years: int, columns: list, title='esqueceu o titulo kkkkkk', plot=True): # Forecasting exclusivam
+        # Criando um dataframe com as colunas que o usuário escolheu
+        df = pd.DataFrame()
+        for column in columns:
+            df = pd.concat([df, self[column[0]][column[1]]], axis=1)
+        training_data = df[:training_years] ## splicing do dataframe
 
-
+        if training_years == len(df):
+            pass # Para o futuro
+        
+        # Código de projeção
+        model = sm.tsa.VAR(np.asarray(training_data, dtype='float'))
+        model_fit = model.fit()
+        prediction = pd.DataFrame(model_fit.forecast(model.endog, steps=(len(df)-training_years))) ## gerado o dataframe com a projeção dos próximos anos
+        prediction.index = [training_data.index[-1] + pd.offsets.DateOffset(years=(i+1)) for i in range(len(prediction))]
+        prediction.rename(columns={i: name for i, name in enumerate(df.columns.values)}, inplace=True)
+        forecast = pd.concat([training_data, prediction])
+        forecast.rename(columns={name: (name + ' projetado') for name in forecast.columns.values}, inplace=True)
+        result = pd.concat([forecast, df], axis=1)
+        result = self.change_to_DataTable(result, title)
+        if plot:
+            self.plot_selection(df=result)
+        return result
+    
     def add_dataframe(self, df, name):
         i = 0 
         while name in self.names:
             i += 1
             name = f'{name}_{i}'
-        dt =  DataTable(columns = df.columns, data = copy.deepcopy(df.values), name=name)
+        dt = self.change_to_DataTable(df, name=name)
         self.datas[name] = dt
         self.names.append(name)
 
+    def change_to_DataTable(self, df, name):
+        df_copy = DataTable(columns = df.columns, data = copy.deepcopy(df.values), name=name)
+        return df_copy
+    
 
+    
 class DataTable(pd.DataFrame):
     def __init__(self, name, **kwargs):
         super().__init__(**kwargs)
         self.name = name
-        self['ANO'] = pd.to_datetime(self['ANO'], format='%Y')
         if self.index.name != 'ANO' and ('ANO' in self.columns):
+            self['ANO'] = pd.to_datetime(self['ANO'], format='%Y')
             self.set_index('ANO', drop=True, inplace=True)
         else:
-            self.index = pd.to_datetime(pd.Series([ano for ano in range(1970, 2023)]))
+            self.index = pd.to_datetime(pd.Series([ano for ano in range(1970, 2023)]), format='%Y')
             self.index.name='ANO'
 
